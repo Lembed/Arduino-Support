b671e32cd67b5d61a7413cf5b714ad52eb0c79e8
cw7 <support@lembed.org>
2016-08-14 23:05:12 +0800

    board defined modified

--- hardware/lembed/arm/cores/maple/Print.cpp
+++ hardware/lembed/arm/cores/maple/Print.cpp
@@ -46,60 +46,69 @@
  * Public methods
  */
 
-size_t Print::write(const char *str) {
-	size_t n = 0;
+size_t Print::write(const char *str)
+{
+    size_t n = 0;
     while (*str) {
         write(*str++);
-		n++;
+        n++;
     }
-	return n;
+    return n;
 }
 
-size_t Print::write(const void *buffer, uint32 size) {
-	size_t n = 0;
+size_t Print::write(const void *buffer, uint32 size)
+{
+    size_t n = 0;
     uint8 *ch = (uint8*)buffer;
     while (size--) {
         write(*ch++);
     }
-	return n;
+    return n;
 }
 
-size_t Print::print(uint8 b, int base) {
+size_t Print::print(uint8 b, int base)
+{
     return print((uint64)b, base);
 }
 
 size_t Print::print(const String &s)
 {
-  return write(s.c_str(), s.length());
+    return write(s.c_str(), s.length());
 }
 
-size_t Print::print(char c) {
+size_t Print::print(char c)
+{
     return write(c);
 }
 
-size_t Print::print(const char str[]) {
+size_t Print::print(const char str[])
+{
     return write(str);
 }
 
-size_t Print::print(int n, int base) {
+size_t Print::print(int n, int base)
+{
     return print((long long)n, base);
 }
 
-size_t Print::print(unsigned int n, int base) {
+size_t Print::print(unsigned int n, int base)
+{
     return print((unsigned long long)n, base);
 }
 
-size_t Print::print(long n, int base) {
+size_t Print::print(long n, int base)
+{
     return print((long long)n, base);
 }
 
-size_t Print::print(unsigned long n, int base) {
+size_t Print::print(unsigned long n, int base)
+{
     return print((unsigned long long)n, base);
 }
 
-size_t Print::print(long long n, int base) {
-    if (base == BYTE) 
-	{
+size_t Print::print(long long n, int base)
+{
+    if (base == BYTE) {
         return write((uint8)n);
     }
     if (n < 0) {
@@ -109,92 +118,104 @@ size_t Print::print(long long n, int base) {
     return printNumber(n, base);
 }
 
-size_t Print::print(unsigned long long n, int base) {
-size_t c=0;
+size_t Print::print(unsigned long long n, int base)
+{
+    size_t c = 0;
     if (base == BYTE) {
-        c= write((uint8)n);
+        c = write((uint8)n);
     } else {
-        c= printNumber(n, base);
+        c = printNumber(n, base);
     }
-	return c;
+    return c;
 }
 
-size_t Print::print(double n, int digits) {
+size_t Print::print(double n, int digits)
+{
     return printFloat(n, digits);
 }
 
-size_t Print::println(void) 
+size_t Print::println(void)
 {
-	size_t n =  print('\r');
+    size_t n =  print('\r');
     n += print('\n');
-	return n;
+    return n;
 }
 
 size_t Print::println(const String &s)
 {
-  size_t n = print(s);
-  n += println();
-  return n;
+    size_t n = print(s);
+    n += println();
+    return n;
 }
 
-size_t Print::println(char c) {
+size_t Print::println(char c)
+{
     size_t n = print(c);
     n += println();
-	return n;
+    return n;
 }
 
-size_t Print::println(const char c[]) {
+size_t Print::println(const char c[])
+{
     size_t n = print(c);
     n += println();
-	return n;
+    return n;
 }
 
-size_t Print::println(uint8 b, int base) {
+size_t Print::println(uint8 b, int base)
+{
     size_t n = print(b, base);
-	n += println();
-	return n;
+    n += println();
+    return n;
 }
 
-size_t Print::println(int n, int base) {
+size_t Print::println(int n, int base)
+{
     size_t s = print(n, base);
     s += println();
-	return s;
+    return s;
 }
 
-size_t Print::println(unsigned int n, int base) {
+size_t Print::println(unsigned int n, int base)
+{
     size_t s = print(n, base);
     s += println();
-	return s;
+    return s;
 }
 
-size_t Print::println(long n, int base) {
+size_t Print::println(long n, int base)
+{
     size_t s = print((long long)n, base);
     s += println();
-	return s;
+    return s;
 }
 
-size_t Print::println(unsigned long n, int base) {
+size_t Print::println(unsigned long n, int base)
+{
     size_t s = print((unsigned long long)n, base);
     s += println();
-	return s;
+    return s;
 }
 
-size_t Print::println(long long n, int base) {
+size_t Print::println(long long n, int base)
+{
     size_t s = print(n, base);
     s += println();
-	return s;
+    return s;
 }
 
-size_t Print::println(unsigned long long n, int base) {
+size_t Print::println(unsigned long long n, int base)
+{
     size_t s = print(n, base);
     s += println();
-	return s;
+    return s;
 }
 
-size_t Print::println(double n, int digits) {
+size_t Print::println(double n, int digits)
+{
     size_t s = print(n, digits);
     s += println();
-	return s;
+    return s;
 }
 
 #ifdef SUPPORTS_PRINTF
@@ -203,27 +224,28 @@ size_t Print::println(double n, int digits) {
 // Work in progress to support printf.
 // Need to implement stream FILE to write individual chars to chosen serial port
 int Print::printf (__const char *__restrict __format, ...)
- {
-FILE *__restrict __stream;
-     int ret_status = 0;
+{
+    FILE *__restrict __stream;
+    int ret_status = 0;
 
 
-     va_list args;
-     va_start(args,__format);
-     ret_status = vfprintf(__stream, __format, args);
-     va_end(args);
-     return ret_status;
- }
- #endif
+    va_list args;
+    va_start(args, __format);
+    ret_status = vfprintf(__stream, __format, args);
+    va_end(args);
+    return ret_status;
+}
+#endif
 
 /*
  * Private methods
  */
 
-size_t Print::printNumber(unsigned long long n, uint8 base) {
+size_t Print::printNumber(unsigned long long n, uint8 base)
+{
     unsigned char buf[CHAR_BIT * sizeof(long long)];
     unsigned long i = 0;
-	size_t s=0;
+    size_t s = 0;
     if (n == 0) {
         print('0');
         return 1;
@@ -236,10 +258,10 @@ size_t Print::printNumber(unsigned long long n, uint8 base) {
 
     for (; i > 0; i--) {
         s += print((char)(buf[i - 1] < 10 ?
-                     '0' + buf[i - 1] :
-                     'A' + buf[i - 1] - 10));
+                          '0' + buf[i - 1] :
+                          'A' + buf[i - 1] - 10));
     }
-	return s;
+    return s;
 }
 
 
@@ -260,20 +282,21 @@ size_t Print::printNumber(unsigned long long n, uint8 base) {
  *
  * http://kurtstephens.com/files/p372-steele.pdf
  */
-size_t Print::printFloat(double number, uint8 digits) {
-size_t s=0;
+size_t Print::printFloat(double number, uint8 digits)
+{
+    size_t s = 0;
     // Hackish fail-fast behavior for large-magnitude doubles
     if (abs(number) >= LARGE_DOUBLE_TRESHOLD) {
         if (number < 0.0) {
-            s=print('-');
+            s = print('-');
         }
-        s+=print("<large double>");
+        s += print("<large double>");
         return s;
     }
 
     // Handle negative numbers
     if (number < 0.0) {
-        s+=print('-');
+        s += print('-');
         number = -number;
     }
 
@@ -288,20 +311,20 @@ size_t s=0;
     // Extract the integer part of the number and print it
     long long int_part = (long long)number;
     double remainder = number - int_part;
-    s+=print(int_part);
+    s += print(int_part);
 
     // Print the decimal point, but only if there are digits beyond
     if (digits > 0) {
-        s+=print(".");
+        s += print(".");
     }
 
     // Extract digits from the remainder one at a time
     while (digits-- > 0) {
         remainder *= 10.0;
         int to_print = (int)remainder;
-        s+=print(to_print);
+        s += print(to_print);
         remainder -= to_print;
     }
-	return s;
+    return s;
 }
 
