820f73eb0151bc84db996ecf45d8f420be7a91d0
cw7 <support@lembed.org>
2016-08-15 21:52:15 +0800

    freertos examples

---
+++ examples/arm/FreeRTOS/examples/blink/blink.ino
@@ -0,0 +1,29 @@
+#include <ArduinoFreeRTOS.h>
+
+static void vLEDFlashTask(void *pvParameters) {
+    for (;;) {
+        vTaskDelay(1000);
+        digitalWrite(BOARD_LED_PIN, HIGH);
+        vTaskDelay(50);
+        digitalWrite(BOARD_LED_PIN, LOW);
+    }
+}
+
+void setup() {
+    // initialize the digital pin as an output:
+    pinMode(BOARD_LED_PIN, OUTPUT);
+
+    xTaskCreate(vLEDFlashTask,
+                "Task1",
+                configMINIMAL_STACK_SIZE,
+                NULL,
+                tskIDLE_PRIORITY + 2,
+                NULL);
+    vTaskStartScheduler();
+}
+
+void loop() {
+    // Insert background code here
+}
+
+
